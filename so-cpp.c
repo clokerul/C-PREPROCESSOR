96081f756b9908360b4e1c6b938a0573d42d4c88
clokerul <lazureanugeorge@yahoo.com>
2022-03-21 14:44:55 -0700

    LAST_COMMIT

--- so-cpp.c
+++ so-cpp.c
@@ -18,6 +18,7 @@ int main(int argc, char *argv[]) {
 
     init_lineargs(line_args);
     process_arguments(symbol_map, line_args, argc, argv);
+    out_file = stdout;
 
     // Open input stream
     if (line_args->infile[0] != '\0') {
